c42ca1b6a0d81fa1bf53dea8824b8f2f90cf43fb
khalilnazari <khalil.deve@gmail.com>
2024-10-19 10:28:07 +0800

    tailwind.config updated

--- health-website/tailwind.config.js
+++ health-website/tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        default: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1330px",
+    },
+    extend: {
+      colors: {
+        primary: "#242a2b",
+        secondary: "#808080",
+        accent: {
+          default: "#1cbccf",
+          secondary: "#18abbc",
+          tertiary: "#90c6cd",
+        },
+        grey: "#e8f0f1",
+      },
+      fontFamily: {
+        primary: "Poppins",
+      },
+      boxShadow: {
+        custom1: "0px 2px 40px 0px rgba(8, 70, 78, 0.08)",
+        custom2: "0px 0px 30px 0px rgba(8, 73, 81, 0.06)",
+      },
+      backgroundImage: {
+        services: "url(../../assets/img/services/bg.svg)",
+        testimonials: "url(../../assets/img/testimonials/bg.svg)",
+        departments: "url(../../assets/img/departments/bg.svg)",
+        quoteRight: "url(../../assets/icons/departments/quote-right.svg)",
+      },
+    },
   },
   plugins: [],
 };
