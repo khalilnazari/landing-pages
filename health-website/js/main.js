8637e0ccee36476cf4323ca78fea2ba635548c7a
khalilnazari <khalil.deve@gmail.com>
2024-10-20 12:22:29 +0800

    Mobile nav developed

--- health-website/js/main.js
+++ health-website/js/main.js
@@ -1,1 +1,24 @@
-console.log("hello worl");
+// mobile nave
+const mobileNav = document.querySelector("#mnav");
+const closeBtn = document.querySelector(".mnav__close-btn");
+const closeBtnIcon = document.querySelector(".mnav__close-btn-icon");
+
+const openedClass = "left-0";
+const closedClass = "-left-[300px]";
+const arrowLeftClass = "ri-arrow-left-s-line";
+const arrowRightClass = "ri-arrow-right-s-line";
+
+closeBtn.addEventListener("click", () => {
+  if (mobileNav.classList.contains(closedClass)) {
+    mobileNav.classList.remove(closedClass);
+    mobileNav.classList.add(openedClass);
+
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  } else {
+    mobileNav.classList.add(closedClass);
+    mobileNav.classList.remove(openedClass);
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  }
+});
